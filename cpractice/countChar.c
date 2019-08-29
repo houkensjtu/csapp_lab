f040a22b2e9eda25c3a1fe84d493f30897bb2b81
houkensjtu <houkensjtu@gmail.com>
2019-08-28 23:05:34 -0400

    Revival!

--- cpractice/countChar.c
+++ cpractice/countChar.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-#include <string.h>
 
-int main(int argc, char const *argv[])
-{
-   char buffer[256];
-
-   printf("Enter your name and press [Enter]:\n");
-   fgets ( buffer, 256 , stdin );
-
-   printf("Your name has %d characters and spaces!\n",
-           strlen( buffer ));
-
-   return 0;
-}
